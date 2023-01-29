9753ae077d2235b46e73b8eaec67f47483de0a2d
alliehong <allie@hongseon-yeong-ui-MacBookPro.local>
2023-01-29 20:53:49 +0900

    add marker on the map

--- src/components/Directions.js
+++ src/components/Directions.js
@@ -5,10 +5,16 @@ const Directions = () => {
     useEffect(()=>{
         const container = document.getElementById('map');
         const options = {
-            center: new kakao.maps.LatLng(33.450701, 126.570667),
-            level: 3
+            center: new kakao.maps.LatLng(36.6065475, 127.4209197),
+            level: 4
         };
         const map = new kakao.maps.Map(container, options);
+        const markerPosition  = new kakao.maps.LatLng(36.6065475, 127.4209197); 
+        const marker = new kakao.maps.Marker({
+            position: markerPosition
+        });
+        marker.setMap(map);
+
     }, [])
     
     return (
